c5f32b0ff16d3405e066811cc5d62ac9d18619da
Alex BOTZUNG <alexandre.botzung@grandest.fr>
2023-12-14 21:21:53 +0100

    Re add framebuffer-vncserver to FOGUefi

--- tools/compile_framebuffer-vncserver.sh
+++ tools/compile_framebuffer-vncserver.sh
@@ -2,15 +2,21 @@
 
 cmake --version > /dev/null 2>&1
 if [ $? -ne "0" ]; then
-	echo "ERROR ! framebuffer-vncserver requires cmake to be compiled. (apt install cmake)"
+	echo "ERROR ! framebuffer-vncserver requires cmake AND libvncserver-dev to be compiled. (apt install cmake libvncserver-dev)"
 	exit 1
 fi
 [ ! -d "framebuffer-vncserver" ] && eval "echo 'ERROR : framebuffer-vncserver does not exist.';exit 1"
-cd "framebuffer-vncserver"
+cd "framebuffer-vncserver" || exit
 [ -d "build" ] && rm -rf "./build"
-[ ! -d "build" ] && mkdir "build"
-cd "./build"
+mkdir "build"
+cd "./build" || exit
 cmake .. || echo "ERROR, compilation failed ! framebuffer-vncserver requires libvncserver-dev to be compiled. (apt install libvncserver-dev)"
 make
+
+[ ! -d "../../fogbuilder/project/bin" ] && mkdir ../../fogbuilder/project/bin
+cp -fv "framebuffer-vncserver" ../../fogbuilder/project/bin
+chmod +x ../../fogbuilder/project/bin
+cd ".." || exit
+[ -d "build" ] && rm -rf "./build"
 
 echo "Done !"
